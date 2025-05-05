d03647a8b4fb7f8529781af17c97040d32d2b09d
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-05-05 18:34:56 +0530

    add start screen with player1 board and randomize ships

---
+++ src/UI/showPlayer1Board.js
@@ -0,0 +1,31 @@
+import { GameManager, UIManager } from "./../manager/barrel";
+import { playerBoard1 } from "./UIcomponents";
+
+export default function showPlayer1Board() {
+  while (playerBoard1.firstChild) {
+    playerBoard1.removeChild(playerBoard1.firstChild);
+  }
+  const userBoard = GameManager.gameBoard1.gameBoard;
+  const ships = GameManager.gameBoard1.ships;
+  const shipCoords = [];
+  ships.forEach((ship) => {
+    ship.positions.forEach((coord) => {
+      shipCoords.push(coord);
+    });
+  });
+
+  for (let i = 0; i < userBoard.length; i++) {
+    for (let j = 0; j < userBoard.length; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      let coord = `${i}${j}`;
+      cell.dataset.coord = coord;
+      if (shipCoords.includes(coord)) {
+        cell.classList.add("ship");
+      }
+      playerBoard1.appendChild(cell);
+    }
+  }
+}
+
+UIManager.subscribe(showPlayer1Board);
--- src/UI/showStartBoard.js
+++ src/UI/showStartBoard.js
@@ -1,19 +1,30 @@
-import { GameManager } from "./../manager/barrel";
+import { GameManager, UIManager } from "./../manager/barrel";
 import {
   startGameButton,
   randomizeButton,
   playComputerButton,
   playPlayerButton,
 } from "./UIcomponents";
+import {showPlayer1Board} from "./barrel"
 
 export default function showStartBoard() {
-  GameManager.setBoardRandomly();
 
-  startGameButton.addEventListener("click", () => {});
+  GameManager.setBoardRandomly();
+  showPlayer1Board();
+
+  startGameButton.addEventListener("click", () => {
+    UIManager.unsubscribe(showPlayer1Board);
+  });
 
   playComputerButton.addEventListener("click", () => {});
 
   playPlayerButton.addEventListener("click", () => {});
 
-  randomizeButton.addEventListener("click", () => {});
+  randomizeButton.addEventListener("click", () => {
+    GameManager.gameBoard1.removeAllShips();
+    GameManager.gameBoard1.placeShipsRandomly();
+    UIManager.notify();
+  });
 }
+
+
