1f975a10b7c7473f6aab64cc40f7b2c31f17ff87
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-05-05 18:03:19 +0530

    fetch and add UI components to seperate file

---
+++ src/UI/UIcomponents.js
@@ -0,0 +1,23 @@
+const commandText = document.querySelector(".command");
+const playerBoard1 = document.querySelector(".board1");
+const playerBoard2 = document.querySelector(".board2");
+const startGameButton = document.querySelector(".start-game-button");
+const randomizeButton = document.querySelector(".randomize-button");
+const resetButton = document.querySelector(".reset-button");
+const choosePlayerDialog = document.querySelector(".choose-player");
+const playComputerButton = document.querySelector(".play-computer");
+const playPlayerButton = document.querySelector(".play-player2");
+const arrangeButton = document.querySelector(".arrange-button");
+
+export {
+  commandText,
+  playerBoard1,
+  playerBoard2,
+  startGameButton,
+  randomizeButton,
+  resetButton,
+  choosePlayerDialog,
+  playComputerButton,
+  playPlayerButton,
+  arrangeButton,
+};
