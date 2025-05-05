9948962050e2aadac0c0449258d8fcc386b96980
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2025-05-05 09:01:28 +0530

    create game manager to manage game

--- src/manager/GameManager.js
+++ src/manager/GameManager.js
@@ -3,13 +3,16 @@ import { Player } from "../models/barrel";
 export default class GameManager {
   player1;
   player2;
+  gameBoard1;
+  gameBoard2;
+  player1turn = false;
+  player2turn = false;
 
-  constructor() {
-    this.player1 = new Player("player1");
-    this.player2 = new Player("computer");
+  constructor(player1, player2 = "computer"){
+    this.player1 = new Player(player1);
+    this.player2 = new Player(player2);
+    this.gameBoard1 = this.player1.playerBoard;
+    this.gameBoard2 = this.player2.playerBoard;
   }
 
-  set opponent(name) {
-    this.player2.playerName = name;
-  }
 }
